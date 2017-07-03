76bd75fb2358d36fb92e62081dd8d04d2fe4876c
Andrew Mirsky <andrew@mirsky.net>
2017-07-03 15:12:40 -0400

    working on home page

---
+++ _includes/octoprint.md
@@ -0,0 +1,9 @@
+---
+---
+
+This is some text about octoprint
+
+this is some other text about octoprint
+
+this can be even more about octoprint
+
--- js/home.js
+++ js/home.js
@@ -1,8 +1,19 @@
-
-
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['onesection', 'twosection', 'threesectin'],
-        sectionSelector: 'section'
+        sectionSelector: 'section',
+        fixedElements: 'nav, #splash',
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+        menu: '#menu',
+        paddingTop: '250px',
+        onLeave: function(index, nextIndex, direction) {
+            if(index===1) {
+                $('#splash').addClass('partial');
+            }
+
+            if(nextIndex === 1) {
+                $('#splash').removeClass('partial');
+            }
+        }
+
     });
 });
